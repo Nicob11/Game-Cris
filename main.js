6f4284e894af905792f09922a6d847c829ba33f4
NicoB <nicobasma_@hotmail.com>
2023-02-09 21:12:50 +0000

    doing a game

--- main.js
+++ main.js
@@ -1,40 +1,96 @@
-import { emojiFoods } from './emoji-foods.js';
-import { shuffleArray } from './utils/shuffleArray.js';
+var cards = [`&#127812`, `&#127765`, `&#127770`, `&#128586`, `&#128584`, `&#128585`, `&#128050`, `&#9996`, `&#127812`, `&#127765`, `&#127770`, `&#128586`, `&#128584`, `&#128585`, `&#128050`, `&#9996`];
+var mixCards = cards.sort(function () {
+    return Math.random() - 0.5;
+});
+//console.log(mixCards)
+//HTML VARIABLES
+//let containerCards = document.querySelector('#cards-container');
+let startGame = document.querySelector('#start');
+let startHints = document.getElementById('hints');
+let timer = document.querySelector('#timer');
+let finishGame = document.querySelector('#finish-game');
+//cartas
+let btnNumber = null;
+let card1 = null;
+let card2 = null;
+let newHints = 0;
+let imgCard = null;
+let click = 0;
+let time = 30;
+let times = false;
+let firstSeleccion = null;
+let secondSeleccion = null;
+let comprobando = false;
 
-// INDICACIONES:
-/**
- *
- * Empezar el juego cuando el usuario haga click en el botón
- * .start-btn
- *
- * Crear las cards con los emojis usando el template
- * #template-card
- *
- * Cada board generado debe ser aleatorio y tiene que estar barajado con un máximo de 16 cards 4x4
- * shuffleArray
- *
- * Añadir clase flipped a las cards cuando el usuario haga click
- * .board (delegación de eventos)
- *
- * Comprobar si las dos cartas son iguales y añadir clase match
- * .match
- *
- * Sumar un punto si son iguales y restar un punto si no lo son
- * Actualizar el contador de puntos
- * .score-board__item-score
- *
- * Iniciar el temporizador cuando se empieza el juego
- * Actualizar el contador de segundos
- * .score-board__item-time
- *
- * Mostrar el mensaje de fin de juego cuando hagan match todas las cards
- * .finish-display
- *
- * Cuando el usuario quiera volver a jugar: ocultar mensaje fin de juego, resetear contador de puntos, segundos y board
- *
- *
- * ⭐BONUS⭐: Compartir en redes sociales que has terminado el proyecto
- * Haz ctrl + click en este link para compartir en Twitter:
- * https://twitter.com/intent/tweet?url=https%3A%2F%2Fnaviscode.gumroad.com%2Fl%2Ffrontend-fastlane-plan&text=Completado%20el%20proyecto%20del%20curso%20del%20DOM%20del%20Frontend%20Fastlane%20Plan%20de%20%40NavisCode%0A%0A%F0%9F%9A%80%20A%20por%20el%20siguiente%20reto%21%0A%0A
- *
- */
+//TIMER START FUNCTION
+
+setInterval(() => {
+    if (times === true) countTime();
+}, 1000);
+
+//START GAME
+startGame.addEventListener('click', () => {
+    if (times == false) {
+        countTime();
+        times = true;
+    }
+})
+//counting time
+function countTime() {
+    time--;
+    timer.innerHTML = `${time}`;
+    if (time === 0) {
+        timer = false;
+        //clearInterval(reverseTime);
+        //blockCard()
+    }
+}
+console.log(mixCards);
+//BTN SELECCIONADO
+document.getElementById('cards-container').addEventListener('click', selectBtn);
+
+function selectBtn(event) {
+    if(comprobando){return}
+    click++;
+    console.log(`click`, click)
+    btnNumber = event.target.id;
+    imgCard = mixCards[btnNumber];
+    if(click == 1 ) showFirtsCard();
+    if(click ==2) showSecondCard();
+}
+//SHOW CARD 1
+function showFirtsCard() {
+        card1 = document.getElementById(`${btnNumber}`);
+        firstSeleccion = card1.innerHTML = `${imgCard}`;
+        card1.disabled = true;
+}
+//SHOW CARD 2
+function showSecondCard() {
+    card2 = document.getElementById(`${btnNumber}`);
+    secondSeleccion = card2.innerHTML = `${imgCard}`;
+    card2.disabled = true;
+    click = 0;
+    cardCompare();
+}
+function cardCompare() {
+    if (firstSeleccion == secondSeleccion) {
+        console.log(`este es el if`);
+        newHints++;
+        startHints.innerHTML = `${newHints}`;
+    } else { //if(firstSeleccion != secondSeleccion){
+        console.log(`este es el else`);
+        firstSeleccion = null;
+        secondSeleccion = null;
+        comprobando = true;
+        setTimeout(() => {
+            card1.innerHTML = '';
+            card1.disabled = false;
+            card2.innerHTML = '';
+            card2.disabled = false;
+            comprobando = false;
+        }, 1500);
+        newHints--;
+        startHints.innerHTML = `${newHints}`;
+    }
+}
+
